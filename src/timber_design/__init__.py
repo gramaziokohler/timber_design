da80f02bfc82bbd173a52b8ffd67aa6cd3379cc1
Chen Kasirer <ckasirer@ethz.ch>
2026-07-02 16:09:56 +0200

    Bump version to 0.3.0

--- src/timber_design/__init__.py
+++ src/timber_design/__init__.py
@@ -7,7 +7,7 @@ __author__ = ["Chen Kasirer"]
 __copyright__ = "Gramazio Kohler Research"
 __license__ = "MIT License"
 __email__ = "kasirer@arch.ethz.ch"
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 
 HERE = os.path.dirname(__file__)
